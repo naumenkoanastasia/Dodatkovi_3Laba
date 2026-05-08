e892708aa181f7ead2d4534254ef37f26c0df63c
𝓐𝓷𝓪𝓼𝓽𝓪𝓼𝓲𝓪 ♡ <naumenko.anastasiia1125@vu.cdu.edu.ua>
2026-05-08 19:29:44 +0300

    Update Naumenko.cs

--- Naumenko.cs
+++ Naumenko.cs
@@ -1,37 +1,36 @@
-﻿using System;
+using System;
 
-namespace ArrayApp
+public static class Naumenko
 {
-    class Program
+
+    public static int[] Naumenko_blok1(ref int[] a)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("ВИКОНАННЯ БЛОКУ 1(Науменко)");
+        Console.WriteLine("Знищити T елементів, починаючи з номеру К (лише якщо всі такі елементи фактично є в масиві)");
 
-        public static void Naumenko_blok1(ref int[] a, int k, int t)
+        int[] array1D = a;
+
+        Console.Write("Введіть індекс K (з якого видаляти): ");
+        int k = int.Parse(Console.ReadLine());
+        Console.Write("Введіть кількість T (скільки видалити): ");
+        int t = int.Parse(Console.ReadLine());
+
+
+        if (a == null || k < 0 || t < 0 || k + t > a.Length)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("ВИКОНАННЯ БЛОКУ 1(Науменко)");
-
-            int[] array1D = Choose();
-
-            Console.Write("Введіть індекс K (з якого видаляти): ");
-            int k = int.Parse(Console.ReadLine());
-            Console.Write("Введіть кількість T (скільки видалити): ");
-            int t = int.Parse(Console.ReadLine());
-
-            Console.ReadLine();
-            if (a == null || k < 0 || t < 0 || k + t > a.Length)
-            {
-                Console.WriteLine("Помилка: некоректні параметри видалення.");
-                return;
-            }
-
-            for (int i = k; i < a.Length - t; i++)
-            {
-                a[i] = a[i + t];
-            }
-
-            Array.Resize(ref a, a.Length - t);
+            Console.WriteLine("Помилка: некоректні параметри видалення.");
+            return a;
         }
 
-
+        for (int i = k; i < a.Length - t; i++)
+        {
+            a[i] = a[i + t];
         }
+
+        Array.Resize(ref a, a.Length - t);
+        return a;
     }
+
+}
+
